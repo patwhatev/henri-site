dad93094cb1ab269482367b18f46ed8e112f63bc
Maculay Culkin <kcirtapnamreve@mac.mynetworksettings.com>
2025-09-09 16:38:09 -0400

    hamburger and image bump

--- src/Main.js
+++ src/Main.js
@@ -3,7 +3,8 @@ import {
   Routes,
   Route,
   NavLink,
-  HashRouter
+  HashRouter,
+  useLocation
 } from "react-router-dom";
 
 // import Assemblage from "./assemblage/Assemblage";
@@ -16,22 +17,89 @@ import Sculpture from "./sculpture/Sculpture";
 import Assemblage from "./assemblage/Assemblage";
 import Instrumentals from "./instrumentals/Instrumentals";
 
+function Navigation() {
+  const location = useLocation();
+  const [mobileMenuOpen, setMobileMenuOpen] = React.useState(false);
+  
+  const toggleMobileMenu = () => {
+    setMobileMenuOpen(!mobileMenuOpen);
+  };
+
+  const closeMobileMenu = () => {
+    setMobileMenuOpen(false);
+  };
+
+  const isActive = (path) => {
+    return location.pathname.startsWith(path);
+  };
+
+  return (
+    <>
+      <h1></h1>
+      <h1><a href="/#">HENRIALEXANDERLEVY</a></h1>
+      
+      {/* Hamburger Menu Button - Only visible on mobile */}
+      <button className="hamburger-menu" onClick={toggleMobileMenu}>
+        <span></span>
+        <span></span>
+        <span></span>
+      </button>
+
+      {/* Desktop Navigation - Hidden on mobile */}
+      <ul className="header desktop-nav">
+        <li><NavLink to="/paintings">Paintings</NavLink></li>
+        <li><NavLink to="/assemblage">Assemblage</NavLink></li>
+        <li><NavLink to="/paper">Works on paper</NavLink></li>
+        <li><NavLink to="/photos">Photographs</NavLink></li>
+        <li><NavLink to="/sculpture">Sculpture/Furniture</NavLink></li>
+        <li><NavLink to="/books">Books/Zines</NavLink></li>
+      </ul>
+
+      {/* Mobile Navigation Menu */}
+      <div className={`mobile-nav ${mobileMenuOpen ? 'mobile-nav-open' : ''}`}>
+        <ul>
+          <li><NavLink 
+            to="/paintings" 
+            onClick={closeMobileMenu}
+            className={isActive('/paintings') ? 'active' : ''}
+          >Paintings</NavLink></li>
+          <li><NavLink 
+            to="/assemblage" 
+            onClick={closeMobileMenu}
+            className={isActive('/assemblage') ? 'active' : ''}
+          >Assemblage</NavLink></li>
+          <li><NavLink 
+            to="/paper" 
+            onClick={closeMobileMenu}
+            className={isActive('/paper') ? 'active' : ''}
+          >Works on paper</NavLink></li>
+          <li><NavLink 
+            to="/photos" 
+            onClick={closeMobileMenu}
+            className={isActive('/photos') ? 'active' : ''}
+          >Photographs</NavLink></li>
+          <li><NavLink 
+            to="/sculpture" 
+            onClick={closeMobileMenu}
+            className={isActive('/sculpture') ? 'active' : ''}
+          >Sculpture/Furniture</NavLink></li>
+          <li><NavLink 
+            to="/books" 
+            onClick={closeMobileMenu}
+            className={isActive('/books') ? 'active' : ''}
+          >Books/Zines</NavLink></li>
+        </ul>
+      </div>
+    </>
+  );
+}
+
 class Main extends Component {
   render() {
     return (
         <HashRouter>
         <div>
-          <h1></h1>
-          <h1><a href="/#">HENRIALEXANDERLEVY</a></h1>
-          <ul className="header">
-            <li><NavLink to="/paintings">Paintings</NavLink></li>
-            <li><NavLink to="/assemblage">Assemblage</NavLink></li>
-            <li><NavLink to="/paper">Works on paper</NavLink></li>
-            <li><NavLink to="/photos">Photographs</NavLink></li>
-            <li><NavLink to="/sculpture">Sculpture/Furniture</NavLink></li>
-            <li><NavLink to="/books">Books/Zines</NavLink></li>
-            {/* <li><NavLink to="/instrumentals">Instrumentals</NavLink></li> */}
-          </ul>
+          <Navigation />
           <div className="content">
             <Routes>
               <Route path="/" element={<Home />} />
