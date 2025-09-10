422126b56b65d4666ead050f65886fb7df08167b
Maculay Culkin <kcirtapnamreve@mac.mynetworksettings.com>
2025-09-10 17:24:02 -0400

    update books

--- src/books/Books.js
+++ src/books/Books.js
@@ -23,27 +23,80 @@ const invite_24 = [
   "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/7072e308-2282-4077-b24b-77ab8615b541/InviteBoxOpen.png?content-type=image%2Fpng",
 ]
 
-const books_arr = [dictatorship, christaine, invite_24]
+const ah_19_invite = [
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/db7e4793-e9ec-4b8f-999f-039a54906c57/AH19FrontCover.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/ad24fad0-097c-4f1f-bd1c-d088e51fe639/AH19Spread.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/06126b53-52fa-4f78-ae2b-0170b5ef8f9c/AH19BackCover.png?content-type=image%2Fpng",
+]
+
+const pe_18_zine = [
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/a9971225-8bfb-409f-b34c-1437553338d5/PE18FrontCover.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/a9059f3a-bd00-483c-b9a1-6db70987bb18/PE18Spread1.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/b819c7de-1004-4622-84a7-72aeceaa5672/PE18Spread2.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/b83780dd-7f2e-4a81-93e5-16fabfc5421c/PE18BackCover.png?content-type=image%2Fpng",
+]
+
+const reverse_keith = [
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/2217077e-20d0-475a-a304-88dec020db16/ReverseKeithRichardsBoxCover.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/10359536-398a-4d96-a9fa-d5b57fa58481/ReverseKeithRichardsBookCover.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/a9467a55-2764-498e-bcb5-ab4ec97cc2d5/ReverseKeithRichardsBook_Spead01.jpg?content-type=image%2Fjpeg",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/e938b4db-83e9-403a-b1d9-bd7f4c1ea128/ReverseKeithRichardsBook_Spead02.jpg?content-type=image%2Fjpeg",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/1a4de605-109a-400e-9903-79ef97a619df/ReverseKeithRichardsBookBack.png?content-type=image%2Fpng"
+]
+
+
+
+const books_arr = [dictatorship, christaine, invite_24, ah_19_invite, pe_18_zine, reverse_keith]
 
 class Books
  extends Component {
+
+  check_img(img) {
+    let style_add = {};
+
+    if (img.includes("/Invite1") || img.includes("/Invite2") || img.includes("/Invite3")) {
+      console.log("found match: ", img)
+      style_add.objectFit = 'cover';
+      style_add.height = '700px';
+      style_add.width = '1000px';
+    } else if ( img.includes("/InviteBox")) {
+      style_add.objectFit = 'cover';
+      style_add.height = '900px';
+      style_add.width = '900px';
+    } else if ( img.includes("/ReverseKeithRich")) {
+      style_add.objectFit = 'contain';
+      style_add.height = '900px';
+      style_add.maxWidth = '840px';
+    } else {
+      style_add.objectFit = 'cover';
+      style_add.width = '700px';
+    }
+
+    return style_add;
+  }
   
   render() {
-    const allImages = books_arr.flat();
-    return (
-      <div className="content">
-          {allImages.map((image, index) => (
+  const allImages = books_arr.flat();
+  return (
+    <div className="content">
+        {allImages.map((image, index) => {
+          const dynamicStyle = this.check_img(image);
+          return (
             <div key={index}>
-              <img className="photo-grid-item" src={image} alt="" style={{
-                width: '600px',
-                height: '600px',
-                objectFit: 'cover'
-              }}  />
+              <img 
+                className="photo-grid-item" 
+                src={image} 
+                alt="" 
+                style={{
+                  ...dynamicStyle
+                }}  
+              />
             </div>
-          ))}         
-      </div>
-    );
-  }
+          );
+        })}         
+    </div>
+  );
+}
 }
 
 export default Books;
--- src/sculpture/ChaiseManteau.js
+++ src/sculpture/ChaiseManteau.js
@@ -1,11 +1,19 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Carousel from 'react-bootstrap/Carousel'
 import React, { Component } from "react";
 
-const imgs = [
+const imgs_old = [
       "https://static1.squarespace.com/static/5ad4d6c7a2772c84c876a433/5d9bfe1d891ec13e8f7d9642/5d9bfe4379967c1670297d4b/1570504284199/IMG_6044.TIF.png?format=2500w",
       "https://static1.squarespace.com/static/5ad4d6c7a2772c84c876a433/5d9bfe1d891ec13e8f7d9642/5d9bfeae9d666c083dbc529d/1570504381169/IMG_6043.jpg.png?format=2500w",
       "https://static1.squarespace.com/static/5ad4d6c7a2772c84c876a433/5d9bfe1d891ec13e8f7d9642/5d9bfeb47161583d7ba1033c/1570504387810/IMG_6042.jpg.png?format=2500w",
       "https://static1.squarespace.com/static/5ad4d6c7a2772c84c876a433/5d9bfe1d891ec13e8f7d9642/5d9bfeb47161583d7ba1033c/1570504387810/IMG_6042.jpg.png?format=2500w"
+];
+
+const imgs = [
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/d2120b41-4bdb-4306-abb5-31fbefd9f5a4/CornerChair1.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/e87d6ba6-46c3-467a-8536-7b8ee859857f/CornerChair2.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/0271cde4-e834-431f-85e4-00ac6aad5f62/SingleCrossChair1.png?content-type=image%2Fpng",
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/c2362228-3492-4f2b-af70-e142514c29f8/SingleCrossChair2.png?content-type=image%2Fpng"
 ];
 
 class ChaiseManteau
@@ -13,9 +21,13 @@ class ChaiseManteau
   render() {
     return (
       <div>
+          <Carousel>
           {imgs.map(image => (
-              <img className="d-block w-100 scrolldown-img" src={image} alt="" />
+            <Carousel.Item>
+              <img className="d-block w-100" src={image} alt="" />
+            </Carousel.Item>
           ))}         
+        </Carousel>        
       </div>
     );
   }
