674db3b360463239553e0cfd615da6999fcf3d52
Maculay Culkin <kcirtapnamreve@mac.mynetworksettings.com>
2025-09-11 22:05:42 -0400

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
-import Main from "./Main";
-import "./index.css";
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<Main />);
