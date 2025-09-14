bb057b3b40a740c2d3194d2e78a7d0d78aa9d61e
Maculay Culkin <kcirtapnamreve@mac.mynetworksettings.com>
2025-09-14 17:36:51 -0400

    rm blk paintings 03

--- src/paintings/BlackPaintings.js
+++ src/paintings/BlackPaintings.js
@@ -5,7 +5,7 @@ import React, { Component } from "react";
 const imgs = [
   "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/cc4dd351-c1e5-47e9-beed-997bef749647/Black+Paintings_01.jpg?content-type=image%2Fjpeg",
   "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/dcab4599-311c-4e8b-bae0-c03d9b1cad8e/Black+Paintings_02.jpg?content-type=image%2Fjpeg",
-  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/9d3676f4-bbff-4c16-9cba-4448e135cd79/Black+Paintings_03.jpg?content-type=image%2Fjpeg",
+  // "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/9d3676f4-bbff-4c16-9cba-4448e135cd79/Black+Paintings_03.jpg?content-type=image%2Fjpeg",
   "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/863e6e65-43da-4c07-b28b-4ab3cf839c1b/Black+Paintings_04.jpg?content-type=image%2Fjpeg",
   "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/05e8ca7d-240d-416c-b100-5b7d3f0b3ebe/Black+Paintings_05.jpg?content-type=image%2Fjpeg",
   "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/190ca95d-f17f-45e0-a468-305aac78fb6a/Black+Paintings_06.jpg?content-type=image%2Fjpeg",
