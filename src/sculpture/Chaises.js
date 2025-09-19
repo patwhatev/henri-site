a3d6fa3b718f6ac7961af77912fbb084faffaeb7
Maculay Culkin <kcirtapnamreve@mac.mynetworksettings.com>
2025-09-18 23:17:36 -0400

    update chair order

--- src/sculpture/Chaises.js
+++ src/sculpture/Chaises.js
@@ -3,8 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React, { Component } from "react";
 
 const imgs = [
+  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/64fd2ecf-d9b1-4f0e-8739-c57d9b5ba0fc/Chaises_02.jpg?content-type=image%2Fjpeg",
   "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/98184176-30d3-450f-a90a-29be862b4237/Chaises_01.jpg?content-type=image%2Fjpeg",
-  "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/64fd2ecf-d9b1-4f0e-8739-c57d9b5ba0fc/Chaises_02.jpg?content-type=image%2Fjpeg"
 ];
 
 class Chaises extends Component {
