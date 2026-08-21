9c18721c7cea0036e236e2ccd176f6708164f02f
Patrick <everman.patrick@gmail.com>
2026-08-21 17:41:35 -0400

    fixed dep vulns and updated player to not rely on archive's player

--- src/instrumentals/Instrumentals.js
+++ src/instrumentals/Instrumentals.js
@@ -6,35 +6,30 @@ const tracks = [
     image: "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/c5019a4e-3636-4553-9a59-e2122e5cb242/normans.png?content-type=image%2Fpng",
     title: "normans medecine circle",
     trackNumber: 1,
-    playerUrl: "https://archive.org/embed/porcelain-trays-2/01-normans%20medecine%20circle%20-%201_22_23%2C%209.08%20PM.m4a",
     audioUrl: "https://archive.org/download/porcelain-trays-2/01-normans%20medecine%20circle%20-%201_22_23%2C%209.08%20PM.m4a"
   },
   {
     image: "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/335cf6bb-69ed-4ce5-afd4-fe931ccf3df6/alchemize.png?content-type=image%2Fpng",
     title: "alchemizemysuffering",
     trackNumber: 2,
-    playerUrl: "https://archive.org/embed/porcelain-trays-2/02-alchemizemysuffering%20-%206_10_25%2C%206.30%20PM.m4a",
     audioUrl: "https://archive.org/download/porcelain-trays-2/02-alchemizemysuffering%20-%206_10_25%2C%206.30%20PM.m4a"
   },
   {
     image: "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/306dfe6b-7383-4834-b090-85bc4dd92ce2/mexique.png?content-type=image%2Fpng",
     title: "mexique",
     trackNumber: 3,
-    playerUrl: "https://archive.org/embed/porcelain-trays-2/03-mexique.m4a",
     audioUrl: "https://archive.org/download/porcelain-trays-2/03-mexique.m4a"
   },
   {
     image: "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/6cd27898-b349-475b-80d3-20e8ff303597/highschool.png?content-type=image%2Fpng",
     title: "buy a girls highschoolV2",
     trackNumber: 4,
-    playerUrl: "https://archive.org/embed/porcelain-trays-2/buy%20a%20girls%20highschoolV2.m4a",
     audioUrl: "https://archive.org/download/porcelain-trays-2/buy%20a%20girls%20highschoolV2.m4a"
   },
   {
     image: "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/fd21f409-991e-40d7-a69a-0286fd1ca694/sunkil.png?content-type=image%2Fpng",
     title: "sunkilhank",
     trackNumber: 5,
-    playerUrl: "https://archive.org/embed/porcelain-trays-2/05-sunkilhank.m4a",
     audioUrl: "https://archive.org/download/porcelain-trays-2/05-sunkilhank.m4a"
   },
 
@@ -43,8 +38,7 @@ const tracks = [
 const porcelain_trays = {
   cover: "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/128ec8c7-c217-4a1f-8d83-b26e58abb9c4/Cover_01.jpg?content-type=image%2Fjpeg",
   back: "https://images.squarespace-cdn.com/content/53667a41e4b0e77173cb3dd1/11471b97-d5d0-4119-8a47-1a90cf5a5301/Cover_02.jpg?content-type=image%2Fjpeg",
-  tracks: tracks,
-  player: "https://archive.org/embed/porcelain-trays-2"
+  tracks: tracks
 }
 
 // const swiss_toys = {
@@ -58,55 +52,35 @@ class Instrumentals extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentlyPlaying: null,
-      isMobile: this.detectMobile()
+      currentlyPlaying: null
     };
-    this.audioRefs = {};
     this.htmlAudioRefs = {};
   }
 
-  detectMobile = () => {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
-  }
-
   playTrack = (albumIndex, track) => {
-    const { currentlyPlaying, isMobile } = this.state;
+    const { currentlyPlaying } = this.state;
     const trackKey = `${albumIndex}-${track.trackNumber}`;
 
-    // Stop any currently playing track
-    if (currentlyPlaying) {
-      if (isMobile) {
-        Object.values(this.htmlAudioRefs).forEach(audio => {
-          if (audio) {
-            audio.pause();
-            audio.currentTime = 0;
-          }
-        });
-      } else {
-        if (this.audioRefs[albumIndex]) {
-          this.audioRefs[albumIndex].src = track.playerUrl;
-        }
+    Object.entries(this.htmlAudioRefs).forEach(([key, audio]) => {
+      if (audio && key !== trackKey) {
+        audio.pause();
+        audio.currentTime = 0;
       }
-    }
+    });
 
     if (currentlyPlaying === trackKey) {
+      const audioElement = this.htmlAudioRefs[trackKey];
+      if (audioElement) {
+        audioElement.pause();
+        audioElement.currentTime = 0;
+      }
       this.setState({ currentlyPlaying: null });
     } else {
-      if (isMobile) {
-        const audioElement = this.htmlAudioRefs[trackKey];
-        if (audioElement) {
-          console.log('Playing track:', track.title, 'URL:', track.audioUrl);
-          audioElement.play().catch(e => {
-            console.log('Audio play failed for track:', track.title, 'Error:', e);
-            console.log('Audio element src:', audioElement.src);
-          });
-        } else {
-          console.log('Audio element not found for track:', trackKey);
-        }
-      } else {
-        if (this.audioRefs[albumIndex]) {
-          this.audioRefs[albumIndex].src = `${track.playerUrl}?auto=1&autoplay=1`;
-        }
+      const audioElement = this.htmlAudioRefs[trackKey];
+      if (audioElement) {
+        audioElement.play().catch(e => {
+          console.log('Audio play failed for track:', track.title, 'Error:', e);
+        });
       }
       this.setState({ currentlyPlaying: trackKey });
     }
@@ -114,7 +88,7 @@ class Instrumentals extends Component {
 
   render() {
     const allAlbums = albums_arr.flat();
-    const { currentlyPlaying, isMobile } = this.state;
+    const { currentlyPlaying } = this.state;
 
     const albumContainerStyle = {
       display: 'flex',
@@ -214,40 +188,18 @@ class Instrumentals extends Component {
                       style={getTrackStyle(isPlaying)}
                       title={track.title}
                     />
-                    {isMobile && (
-                      <audio
-                        ref={ref => this.htmlAudioRefs[trackKey] = ref}
-                        preload="metadata"
-                        style={{ display: 'none' }}
-                        src={track.audioUrl}
-                        onError={(e) => console.log('Audio error for', track.title, ':', e)}
-                        onLoadStart={() => console.log('Loading started for', track.title)}
-                        onCanPlay={() => console.log('Can play', track.title)}
-                      />
-                    )}
+                    <audio
+                      ref={ref => this.htmlAudioRefs[trackKey] = ref}
+                      preload="metadata"
+                      style={{ display: 'none' }}
+                      src={track.audioUrl}
+                      onEnded={() => this.setState({ currentlyPlaying: null })}
+                      onError={(e) => console.log('Audio error for', track.title, ':', e)}
+                    />
                   </div>
                 );
               })}
             </div>
-
-            {!isMobile && (
-              <iframe
-                ref={ref => this.audioRefs[index] = ref}
-                src={album.player}
-                width="1"
-                height="1"
-                frameBorder="0"
-                style={{
-                  position: 'absolute',
-                  left: '-9999px',
-                  opacity: 0,
-                  pointerEvents: 'none'
-                }}
-                webkitallowfullscreen="true"
-                mozallowfullscreen="true"
-                allowFullScreen
-              />
-            )}
           </div>
         ))}
       </div>
